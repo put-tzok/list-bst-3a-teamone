042126bcd5dd428a2e0a8acb3ee139eeaec21480
Sławomir Staniszewski <62444659+SlSta@users.noreply.github.com>
2020-04-25 11:14:14 +0200

    Update list.c

--- list.c
+++ list.c
@@ -4,35 +4,75 @@
 #include <stdlib.h>
 #include <time.h>
 
-unsigned int ns[] = { 10, /* TODO: fill values which will be used as lists' sizes */ };
+unsigned int ns[] = { 1000, 2000, 5000, 7500, 10000, 15000, 20000, 30000, 40000, 50000 };
 
 // each list node contains an integer key value and pointer to next list node
 struct node {
     int key;
-    struct node *next;
+    struct node* next;
 };
 
 // list's beginning is called the head of list
-struct node *head = NULL;
+struct node* head = NULL;
 
 
-struct node* list_insert(int value) {
-    // TODO: implement check
-    return NULL;
+struct node* list_insert(int value) //todo
+{
+    struct node* new_element;
+    new_element = (struct node*) malloc(sizeof(struct node));
+    new_element->key = value;
+    new_element->next = head;
+    head = new_element;
+
+    return new_element;
 }
 
-struct node* list_search(int value) {
-    // TODO: implement
-    return NULL;
+struct node* list_search(int value) //todo
+{
+    struct node* element;
+    element = head;
+    while (element != NULL && element->key != value)
+    {
+        element = element->next;
+    }
+    return element;
 }
 
-void list_delete(int value) {
-    // TODO: implement
+void list_delete(int value) //todo
+{
+    if (head->key == value)
+    {
+        head = head->next;
+    }
+    else
+    {
+        struct node* old;
+        old = head;
+        struct node* new;
+        new = head->next;
+        while (new != NULL && new->key != value)
+        {
+            old = new;
+            new = new->next;
+        }
+        if (new != NULL)
+        {
+            old->next = new->next;
+        }
+    }
 }
 
-unsigned int list_size() {
-    // TODO: implement
-    return 0;
+unsigned int list_size() //todo
+{
+    int size = 0;
+    struct node* point;
+    point = head;
+    while (point != NULL)
+    {
+        size += 1;
+        point = point->next;
+    }
+    return size;
 }
 
 /*
@@ -42,7 +82,7 @@ unsigned int list_size() {
  *      int *t:     pointer to the array
  *      int n:      number of elements in the array
  */
-void fill_increasing(int *t, int n) {
+void fill_increasing(int* t, int n) {
     for (int i = 0; i < n; i++) {
         t[i] = i;
     }
@@ -55,7 +95,7 @@ void fill_increasing(int *t, int n) {
  *      int *t:     pointer to the array
  *      int n:      number of elements in the array
  */
-void shuffle(int *t, int n) {
+void shuffle(int* t, int n) {
     for (int i = n - 1; i > 0; i--) {
         int j = rand() % i;
         int temp = t[i];
@@ -74,7 +114,7 @@ int main() {
             unsigned int n = ns[j];
 
             // always create an array of size `n` and fill it with increasing values
-            int *t = malloc(n * sizeof(*t));
+            int* t = malloc(n * sizeof(*t));
             fill_increasing(t, n);
 
             // if true, reorder array elements randomly
@@ -85,7 +125,7 @@ int main() {
             // insert elements in the order present in array `t`
             clock_t insertion_time = clock();
             for (unsigned int k = 0; k < n; k++) {
-                struct node *iter = list_insert(t[k]);
+                struct node* iter = list_insert(t[k]);
                 assert(iter != NULL);       // inserted element cannot be NULL
                 assert(iter->key == t[k]);  // inserted element must contain the expected value
             }
@@ -97,7 +137,7 @@ int main() {
             // search for every element in the order present in array `t`
             clock_t search_time = clock();
             for (unsigned int k = 0; k < n; k++) {
-                struct node *iter = list_search(t[k]);
+                struct node* iter = list_search(t[k]);
                 assert(iter != NULL);       // found element cannot be NULL
                 assert(iter->key == t[k]);  // found element must contain the expected value
             }
@@ -105,7 +145,6 @@ int main() {
 
             // reorder array elements before deletion
             shuffle(t, n);
-
             // delete every element in the order present in array `t`
             for (unsigned int k = 0, l = n; k < n; k++, l--) {
                 assert(list_size() == l);   // list size must be equal to the expected value
@@ -113,12 +152,11 @@ int main() {
             }
             assert(list_size() == 0);       // after all deletions, the list size is zero
             assert(head == NULL);           // after all deletions, the list's head is NULL
-
             free(t);
 
             printf("%d\t%s\t%f\t%f\n", n, enable_shuffle ? "true" : "false",
-                    (double)insertion_time / CLOCKS_PER_SEC,
-                    (double)search_time / CLOCKS_PER_SEC);
+                (double)insertion_time / CLOCKS_PER_SEC,
+                (double)search_time / CLOCKS_PER_SEC);
         }
     }
     return 0;
